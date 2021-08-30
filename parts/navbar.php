7b4d5690c80a3809c3109885e0783c6d6682de84
Dekpo <dekpo@me.com>
2021-08-30 08:57:42 +0200

    Really small touch...

--- parts/navbar.php
+++ parts/navbar.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
     <a class="navbar-brand" href="index.php">
-      <img src="<?php if (strpos($_SERVER["PHP_SELF"],"adminScred")) echo "." ?>./assets/images/logo.png" alt="Logo Webforce" width="100">
+      <img src="./assets/images/logo.png" alt="Logo Webforce" width="100">
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
